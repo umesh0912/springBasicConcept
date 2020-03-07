c0b47979fb394b4ab72bebdf65dc88da1efb5303
Umesh Jadhav <46950391+umesh0912@users.noreply.github.com>
2020-03-07 19:38:53 +0530

    second commit

--- src/main/java/com/uj/aop/KnightAopMain.java
+++ src/main/java/com/uj/aop/KnightAopMain.java
@@ -7,7 +7,8 @@ import com.uj.interfces.Knight;
 
 public class KnightAopMain {
 	  public static void main(String[] args) {
-	    ApplicationContext context = 
+	    @SuppressWarnings("resource")
+		ApplicationContext context = 
 	        new ClassPathXmlApplicationContext("knights-aop.xml");
 	    
 	    Knight knight = (Knight) context.getBean("knight");
